664829821c1f89e95521d9b12813bcfb41e4bc8b
Isa Ismail <62422922+Isa-Ismail@users.noreply.github.com>
2023-04-06 13:03:36 +0600

    ssad

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -8,7 +8,7 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title style={{ color: "#623686" }}>{props.title}</Card.Title>
+        <Card.Title style={{ color: "#cfbae0" }}>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
