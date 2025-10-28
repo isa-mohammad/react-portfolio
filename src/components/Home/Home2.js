8dd69c8437c6153b900b03a33f831418966327b2
Ismail <62422922+isa-mohammad@users.noreply.github.com>
2025-10-28 22:39:34 +0600

    Update GitHub link in Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -63,7 +63,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/Isa-Ismail"
+                  href="https://github.com/Isa-Mohammad"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
